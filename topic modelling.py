83a9e9d23f488d313bdc947cbacce27a6f92e23a
DhruvS5227 <80899325+DhruvS5227@users.noreply.github.com>
2021-07-29 20:24:36 +0530

    Add files via upload

---
+++ topic modelling.py
@@ -0,0 +1,188 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import tweepy
+import pandas as pd
+import re
+import spacy
+
+
+# In[2]:
+
+
+consumer_key = "ho2tPaRaYSyQsFpA8tx82evZU"
+consumer_secret = "EQkcU19GTKAZ66J3yCLmnGE1aUQNnEMEkUK5y1SXRObMePpSYB"
+access_token = "1399618413262106624-fCJCuarQik1HX8eXGhMAy9pbCKTvdc"
+access_token_secret = "TFaPhwkccwre3eAUUW3d7XIZ1S3seYcA1PKDxpqRdGVdi"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth, wait_on_rate_limit = True)
+
+
+# In[3]:
+
+
+df=[]
+posts= tweepy.Cursor(api.user_timeline,id="LGUS",tweet_mode="extended").items(1000)
+for i in posts:
+    df.append([i.full_text,i.favorite_count,i.created_at])
+
+df=pd.DataFrame(df,columns=['tweets','likes','time'])
+df
+
+
+# In[4]:
+
+
+import string
+def clean_text(text):
+    ''' , and '''
+    text = text.lower()
+    text = re.sub(r'\[.*?\]', '', text)
+    text = re.sub(r'[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub(r'\w*\d\w*', '', text)
+    return text
+  
+tweets_df_clean = pd.DataFrame(df.tweets.apply(lambda x: clean_text(x)))
+tweets_df_clean
+
+
+# In[5]:
+
+
+nlp = spacy.load("en_core_web_lg")
+
+
+# In[6]:
+
+
+def lemmatizer(text):        
+    sent = []
+    doc = nlp(text)
+    for word in doc:
+        sent.append(word.lemma_)
+    return " ".join(sent)
+tweets_df_clean = pd.DataFrame(tweets_df_clean.tweets.apply(lambda x: lemmatizer(x)))
+tweets_df_clean['tweets'] = tweets_df_clean['tweets'].str.replace('-PRON-', '')
+tweets_df_clean['tweets']
+
+
+# In[7]:
+
+
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+import seaborn as sns
+plt.figure(figsize=(10,6))
+doc_lens = [len(d) for d in tweets_df_clean.tweets]
+plt.hist(doc_lens, bins = 100)
+plt.title('Distribution of Tweets character length')
+plt.ylabel('Number of Tweets')
+plt.xlabel('Tweets character length')
+sns.despine();
+
+
+# In[8]:
+
+
+import matplotlib as mpl
+from subprocess import check_output
+import wordcloud
+from wordcloud import WordCloud, STOPWORDS
+mpl.rcParams['figure.figsize']=(12.0,12.0)  
+mpl.rcParams['font.size']=12            
+mpl.rcParams['savefig.dpi']=100             
+mpl.rcParams['figure.subplot.bottom']=.1 
+stopwords = set(STOPWORDS)
+wordcloud = WordCloud(
+                          background_color='white',
+                          stopwords=stopwords,
+                          max_words=500,
+                          max_font_size=40, 
+                          random_state=100
+                         ).generate(str(tweets_df_clean.tweets))
+print(wordcloud)
+fig = plt.figure(1)
+plt.imshow(wordcloud)
+plt.axis('off')
+plt.show();
+
+
+# In[9]:
+
+
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+def get_top_n_words(corpus, n=None):
+    vec = CountVectorizer(stop_words='english').fit(corpus)
+    bag_of_words = vec.transform(corpus)
+    sum_words = bag_of_words.sum(axis=0) 
+    words_freq = [(word, sum_words[0, idx]) for word, idx in vec.vocabulary_.items()]
+    words_freq =sorted(words_freq, key = lambda x: x[1], reverse=True)
+    return words_freq[:n]
+
+common_words = get_top_n_words(tweets_df_clean.tweets, 10)
+unigram = pd.DataFrame(common_words, columns = ['unigram' , 'count'])
+unigram
+
+
+# In[26]:
+
+
+# topic modelling
+
+
+# In[10]:
+
+
+from sklearn.decomposition import LatentDirichletAllocation
+vectorizer = CountVectorizer(
+analyzer='word',       
+min_df=3,
+stop_words='english',
+lowercase=True,
+token_pattern='[a-zA-Z0-9]{3,}',
+max_features=5000,
+                            )
+data_matrix = vectorizer.fit_transform(tweets_df_clean.tweets)
+data_matrix
+
+
+# In[12]:
+
+
+lda_model = LatentDirichletAllocation(
+n_components=10, # Number of topics
+learning_method='online',
+random_state=20,       
+n_jobs = -1  # Use all available CPUs
+                                     )
+lda_output = lda_model.fit_transform(data_matrix)
+
+
+# In[13]:
+
+
+for i,topic in enumerate(lda_model.components_):
+    print(f'Top 10 words for topic #{i}:')
+    print([vectorizer.get_feature_names()[i] for i in topic.argsort()[-10:]])
+    print('\n')
+
+
+# In[14]:
+
+
+import pyLDAvis
+import pyLDAvis.sklearn
+pyLDAvis.enable_notebook()
+pyLDAvis.sklearn.prepare(lda_model, data_matrix, vectorizer, mds='tsne')
+
+
+# In[ ]:
+
+
+
+
